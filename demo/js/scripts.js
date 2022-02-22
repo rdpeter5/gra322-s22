94f883859dca19e5bdf7dc477ee9cbfc866fcb4e
rdpeter5 <97988104+rdpeter5@users.noreply.github.com>
2022-02-22 15:27:21 -0700

    nav button open and close

--- demo/js/scripts.js
+++ demo/js/scripts.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
-    /*
-    finding the nav element and hiding it here this is how you
-    write comments on multiple lines
 
-    // to write commeents on individual lines.
-    */
     $('nav').hide();
 
     // Setup our click function
