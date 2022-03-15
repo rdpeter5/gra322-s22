e2b6e8e7b7137f7c9aa40c64989a5f67ad7b4645
rdpeter5 <97988104+rdpeter5@users.noreply.github.com>
2022-03-15 19:17:08 -0400

    adjust left margin

---
+++ slider/js/scripts.js
@@ -0,0 +1,11 @@
+var slider = tns({
+    container: '.my-slider',
+    nav: false,
+    controls: true,
+    items: 3,
+    slideBy: 'page',
+    autoplay: false,
+    mouseDrag: true,
+    mode: 'gallery',
+    controlsText: ["Previous","Next Image"]
+  });
